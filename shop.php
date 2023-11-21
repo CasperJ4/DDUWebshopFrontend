f929b59aca99dbe036d47075b4c810405ab33d6d
Y0S3FXD <shekyousef@gmail.com>
2023-11-21 12:27:38 +0100

    shop.php

--- shop.php
+++ shop.php
@@ -8,6 +8,30 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 </head>
 <body>
+    <?php include 'header.php'; ?>
+    <?php include 'navbar.php'; ?>
+
+    <div class="container mt-4">
+        <div class="row">
+            <!-- Example of a single product card -->
+            <div class="col-md-4">
+                <div class="card">
+                    <img class="card-img-top" src="path_to_image" alt="Product Image">
+                    <div class="card-body">
+                        <h5 class="card-title">Product Name</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <p class="card-text"><strong>Price:</strong> $XX.XX</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <!-- Repeat for each product -->
+        </div>
+    </div>
+
+    <?php include 'footer.php'; ?>
+</body>
+</html>
 <?php
 include 'navbar.php';
 ?>
