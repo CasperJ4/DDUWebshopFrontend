07d9760d136530f5edc9250f93333011ea87096d
Y0S3FXD <shekyousef@gmail.com>
2023-12-14 12:46:27 +0100

    Har lavet Et admin side
    Et hurtig admin side

---
+++ FrontEnd/admin.php
@@ -0,0 +1,19 @@
+ <div class="container mt-5">
+     <h1 class="text-center">Admin Dashboard</h1>
+     <div class="row mt-4">
+         <div class="col-md-3">
+             <div class="list-group">
+                 <a href="#" class="list-group-item list-group-item-action active">Dashboard</a>
+                 <a href="manage-users.php" class="list-group-item list-group-item-action">Manage Users</a>
+                 <a href="manage-products.php" class="list-group-item list-group-item-action">Manage Products</a>
+                 <a href="manage-orders.php" class="list-group-item list-group-item-action">Manage Orders</a>
+                 <a href="site-settings.php" class="list-group-item list-group-item-action">Site Settings</a>
+             </div>
+         </div>
+         <div class="col-md-9">
+             <h2>Welcome, [Admin Name]</h2>
+             <p>Here you can manage your website's content and settings.</p>
+             <!-- Additional admin functionalities can be added here -->
+         </div>
+     </div>
+ </div>
