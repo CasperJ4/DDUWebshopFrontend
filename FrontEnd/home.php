b8fffcf0094ae283b9c7999d0a0ef8a66ca017bd
Y0S3FXD <shekyousef@gmail.com>
2023-12-14 12:36:35 +0100

    Pull here Design here is almost finshed
    navbar nu is sticky-top man kan alltid se den

---
+++ FrontEnd/home.php
@@ -0,0 +1,72 @@
+<div class="container mt-4 text-center">
+    <div class="row">
+        <!-- Menu -->
+        <div class="col-md-3">
+            <div class="list-group">
+                <a href="#" class="list-group-item list-group-item-action active">Menu</a>
+                <a href="#" class="list-group-item list-group-item-action">Kategori 2</a>
+                <a href="#" class="list-group-item list-group-item-action">Kategori 3</a>
+                <a href="#" class="list-group-item list-group-item-action">Kategori 4</a>
+            </div>
+        </div>
+
+        <!-- Products -->
+        <div class="col-md-9">
+            <div class="row">
+                <!-- Product 1 -->
+                <div class="col-md-4 mb-4">
+                    <!-- Product card -->
+                    <img class="card-img-top" src="https://ih1.redbubble.net/image.2561162615.8891/fposter,small,wall_texture,product,750x1000.jpg" alt="Product Image">
+                    <div class="card-body">
+                        <h5 class="card-title">Product Name</h5>
+                        <p class="card-text">Text Eksample</p>
+                        <p class="card-text"><strong>Price:</strong> 100kr</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <!-- Product 2 -->
+                <div class="col-md-4 mb-4">
+                    <!-- Product card -->
+                    <img class="card-img-top" src="https://ih1.redbubble.net/image.2561162615.8891/fposter,small,wall_texture,product,750x1000.jpg" alt="Product Image">
+                    <div class="card-body">
+                        <h5 class="card-title">Product Name</h5>
+                        <p class="card-text">Text Eksample</p>
+                        <p class="card-text"><strong>Price:</strong> 100kr</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <!-- Product 3 -->
+                <div class="col-md-4 mb-4">
+                    <!-- Product card -->
+                    <img class="card-img-top" src="https://ih1.redbubble.net/image.2561162615.8891/fposter,small,wall_texture,product,750x1000.jpg" alt="Product Image">
+                    <div class="card-body">
+                        <h5 class="card-title">Product Name</h5>
+                        <p class="card-text">Text Eksample</p>
+                        <p class="card-text"><strong>Price:</strong> 100kr</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <!-- Product 4 -->
+                <div class="col-md-4 mb-4">
+                    <!-- Product card -->
+                    <img class="card-img-top" src="https://ih1.redbubble.net/image.2561162615.8891/fposter,small,wall_texture,product,750x1000.jpg" alt="Product Image">
+                    <div class="card-body">
+                        <h5 class="card-title">Product Name</h5>
+                        <p class="card-text">Text Eksample</p>
+                        <p class="card-text"><strong>Price:</strong> 100kr</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <!-- Additional products can be added here -->
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<?php include 'footer.php'; ?>
+</body>
+
+</html>
--- checkout.php
+++ checkout.php
@@ -1,7 +1,5 @@
-<?php include 'header.php'; ?>
-<?php include 'navbar.php'; ?>
-
-                        <?php include 'checkout-content.php'; ?>
-
-
-<?php include 'footer.php'; ?>
+<?php require_once 'start.php'; ?>
+<?php require_once FRONTEND . 'header.php'; ?>
+<?php require_once FRONTEND . 'navbar.php'; ?>
+<?php require_once FRONTEND . 'checkout-content.php'; ?>
+<?php require_once FRONTEND . 'footer.php'; ?>
