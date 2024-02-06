7819589e55589fc46cc022aba2519a29b07c3658
Y0S3FXD <shekyousef@gmail.com>
2024-02-07 00:10:19 +0100

    Fixes

--- FrontEnd/header.php
+++ FrontEnd/header.php
@@ -1,45 +1,45 @@
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
     <title>LeaderFiesta</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+    <!-- DataTables CSS -->
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
+
+    <!-- Custom Styles -->
+    <!-- Append a version number to CSS links to prevent caching issues -->
+    <link rel="stylesheet" type="text/css" href="css/styles.css?v=1.0">
+    <link rel="stylesheet" type="text/css" href="css/checkout.css?v=1.0">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css?v=1.0">
+    <link rel="stylesheet" type="text/css" href="css/cart.css?v=1.0">
+    <link rel="stylesheet" href="css/admin.css?v=1.0">
+    <link rel="stylesheet" href="css/products.css?v=1.0">
+    <link rel="stylesheet" href="css/button.css?v=1.0">
+
+    <!-- Scripts -->
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
-
-
-    <!-- Custom Assets -->
-    <link rel="stylesheet" type="text/css" href="css/styles.css">
-    <link rel="stylesheet" type="text/css" href="css/checkout.css">
-    <link rel="stylesheet" type="text/css" href="css/navbar.css">
-    <link rel="stylesheet" href="css/admin.css">
-    <link rel="stylesheet" href="css/products.css">
-    <link rel="stylesheet" href="css/button.css">
-
-
     <script src="js/cartPopup.js"></script>
-    <!-- Custom Assets -->
 
 </head>
 
 <body>
-    <?php include "cart.php"; ?>
 
+    <!-- Include Cart -->
+    <?php include "cart.php"; ?>
 
     <!-- User Session and Login/Logout -->
     <?php if (isset($_SESSION['user'])) : ?>
-        <?php if (isset($_SESSION['admin'])) : ?>
-        <?php endif; ?>
         <a href="logout.php">
-
             <button class="costum-button"><span>Logout</span></button>
-
         </a>
     <?php else : ?>
         <a href="login.php">
-
             <button class="costum-button"><span>Login</span></button>
-
         </a>
     <?php endif; ?>
